0892df6f4dcb90e69d08da4308c6a58aa73a5627
IweZix <lucanclv2@gmail.com>
2025-11-10 20:18:34 +0100

    fix: migration emplacement

--- src/modules/database/migrations/1762802284885-AddUserTable.ts
+++ src/modules/database/migrations/1762802284885-AddUserTable.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Migrations1762721102900 implements MigrationInterface {
-    name = 'Migrations1762721102900'
+export class AddUserTable1762802284885 implements MigrationInterface {
+    name = 'AddUserTable1762802284885'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstname" character varying NOT NULL, "lastname" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "role" integer NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
